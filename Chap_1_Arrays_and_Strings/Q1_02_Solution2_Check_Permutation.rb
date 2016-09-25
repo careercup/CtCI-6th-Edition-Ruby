9a0355e3f22a41ce1d1ccd2e0c7c69def2b245c6
Jason Sutter <jasonsutter87@gmail.com>
2016-09-24 23:32:12 -0700

    Updated Solution2 Check Permutation Should run O(n)

--- Chap_1_Arrays_and_Strings/Q1_02_Solution2_Check_Permutation.rb
+++ Chap_1_Arrays_and_Strings/Q1_02_Solution2_Check_Permutation.rb
@@ -1,10 +1,8 @@
 def check_permutation_solution2(s1="", s2="")
 		letters = []
 		if s1.length == s2.length
-			s1.split('').each do |char|
-				letters << char
-			end
-
+			letters = s1.split('')
+		
 			s2.split('').each do |char|
 				if letters.include?(char) != true
 					false
