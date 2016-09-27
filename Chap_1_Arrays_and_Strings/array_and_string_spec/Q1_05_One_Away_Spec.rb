70a78242734887ebcb3593deefc5ea08bbc5216e
Hyun Sik Kang <hyunsikk91@gmail.com>
2016-09-26 21:03:17 -0700

    handle case when more than 1 edit away containing same characters

--- Chap_1_Arrays_and_Strings/array_and_string_spec/Q1_05_One_Away_Spec.rb
+++ Chap_1_Arrays_and_Strings/array_and_string_spec/Q1_05_One_Away_Spec.rb
@@ -1,4 +1,4 @@
-require_relative('../Q1_05_One_Array')
+require_relative('../Q1_05_One_Away')
 
 describe 'Checks if two words are one edit away' do
 	let(:pale) {'pale'}
@@ -8,21 +8,26 @@ describe 'Checks if two words are one edit away' do
 	let(:cat) {'cat'}
 	let(:dog) {'dog'}
 	let(:cakess) {'cakess'}
+	let(:elp) {'elp'}
 
 	it 'it will return true if 1 edit away' do
-		expect(one_array(pale, ple)).to eq(true)
+		expect(one_away(pale, ple)).to eq(true)
 	end
 
 	it 'it will return true if 1 edit away' do
-		expect(one_array(cake, bake)).to eq(true)
+		expect(one_away(cake, bake)).to eq(true)
 	end
 
 	it 'it will return false due to over 1 edit away' do
-		expect(one_array(cat, dog)).to eq(false)
+		expect(one_away(cat, dog)).to eq(false)
 	end
 
 	it 'it will return false due to over 1 edit away' do
-		expect(one_array(cake, cakess)).to eq(false)
+		expect(one_away(cake, cakess)).to eq(false)
+	end
+
+	it 'returns false if over 1 edit away' do
+		expect(one_away(pale, elp)).to eq(false)
 	end
 
 end
