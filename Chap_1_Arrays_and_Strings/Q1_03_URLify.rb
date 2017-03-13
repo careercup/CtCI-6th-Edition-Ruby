ded2a45edee1333cfb72375f1b2899b3e8e0c8ad
Sathiyaseelan <ssstarlet@gmail.com>
2017-03-13 12:18:18 +0530

    replaced gsub method with logical approach in Q1_03

--- Chap_1_Arrays_and_Strings/Q1_03_URLify.rb
+++ Chap_1_Arrays_and_Strings/Q1_03_URLify.rb
@@ -1,7 +1,29 @@
-def urlify(string)
-	if string.include?(' ') == true
-		string.gsub!(' ', '%20')
-	else
-		string
+# assume that the string contains greater or eqaul the spaces needed
+def urlify(string, n)
+	chars = string.chars
+	spaces_count = chars[0...n-1].count(' ')
+
+	# For each space, we need 2 more additional spaces to replace %20
+	offset = spaces_count * 2
+
+	# Pointers to keep track the char being traversed (i) & position where to be replaced(j)
+	i = n-1
+	j = i + offset
+
+	while i >= 0
+		if chars[i] != ' '
+			chars[j] = chars[i]
+			j -= 1
+		else
+			chars[j] = '0'
+			chars[j - 1] = '2'
+			chars[j - 2] = '%'
+			j -= 3
+		end
+		i -= 1
 	end
+	chars.join(' ')
 end
+
+# Test code
+puts urlify('Mr John Smith    ', 13)
