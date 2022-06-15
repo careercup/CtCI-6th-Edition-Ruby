27d2e9231dcbd9086af5dfcc82148ea9f7c2cbbf
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-15 18:07:54 +0800

    Delete Chap_3_Stacks_and_Queues directory

--- Chap_3_Stacks_and_Queues/stacks_and_queue_spec/Q3_02_Stack_Min_Spec.rb
+++
@@ -1,9 +0,0 @@
-require_relative('../Q3_02_Stack_Min')
-
-describe 'Find the highest number' do
-	let(:test_array) {[23,34,45,5,5,5,5,5,324,7,8,9,89,0]}
-
-	it 'it should return 324' do
-		expect(higest_num_stack(test_array)).to eq(324)
-	end
-end
